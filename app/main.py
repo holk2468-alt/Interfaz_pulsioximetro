f84dc8ef7e51ea6e41c24efba3cf5ccd86244434
holk2468-alt <holk2468@gmail.com>
2025-09-18 02:51:34 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,14 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes import router
 
 app = FastAPI()
+
+# La configuración de CORS debe ir aquí, justo después de app = FastAPI()
+origins = [
+    "http://localhost:3000",
+    "https://interfaz-pulsioximetro.onrender.com"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
     return {"mensaje": "API del Sistema Médico funcionando 🚀"}
 
-# Montar todas las rutas
 app.include_router(router)
-
-
-
